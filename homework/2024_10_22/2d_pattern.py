75e011d16e4ecb9b97494294fdb67304247de09d
serg_bordiuja <serg.bordiuja@gmail.com>
2024-10-24 10:16:32 +0300

    2d_pattern

--- homework/2024_10_22/2d_pattern.py
+++ homework/2024_10_22/2d_pattern.py
@@ -2,12 +2,35 @@
 
 leg_lenght = 5
 
+# 1) method
+print("\n1) method\n")
+y = 1
+x_counter = 1
+while y <= leg_lenght:
+    print("* ", end="")
+    x_counter += 1
+    if x_counter > y :
+        y += 1
+        x_counter = 1
+        print()
+    
 print("\n")
+
+# 2) method
+print("\n2) method\n")
+
+for y in range(1,leg_lenght + 1):
+    print("* " * y)
+    
+print("\n")
+
+# 3) method
+print("\n3) method\n")
 
 for y in range(1,leg_lenght + 1):
     for x in range(1, y + 1):
         print("* ", end = "")
-    print("\n")
+    print()
 
 
 print("\n")
