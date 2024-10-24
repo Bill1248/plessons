e64ac35bb710af5607e11c9a0385b2861ccd4170
serg_bordiuja <serg.bordiuja@gmail.com>
2024-10-24 09:50:44 +0300

    2d_pattern

---
+++ homework/2024_10_22/2d_pattern.py
@@ -0,0 +1,13 @@
+# 2d draw triangle
+
+leg_lenght = 5
+
+print("\n")
+
+for y in range(1,leg_lenght + 1):
+    for x in range(1, y + 1):
+        print("* ", end = "")
+    print("\n")
+
+
+print("\n")
