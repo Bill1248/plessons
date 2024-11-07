5409e710baa08d7f203e37a330ac69d5ef0c63ed
serg_bordiuja <serg.bordiuja@gmail.com>
2024-11-07 10:34:50 +0200

    restoraunt part 2

--- homework/2024_10_29/Restaraunt/app.py
+++ homework/2024_10_29/Restaraunt/app.py
@@ -1,23 +1,32 @@
-from func import printMenu
+from func import printMenu, printOrder
 from os import system
 
+file = open('data/order.csv', "w")
 while True:
     system("clear")
     data = printMenu()
     pos = int(input("choose -> ")) - 1
+    if pos < 0 :
+        file.close()
+        break
 
-    if pos >= len(data) or pos < 0:
-        print("Wrong position, choose 1 ...", len(data), "Press enter for continue")
+    if pos >= len(data):
+        print("Wrong position, choose 1 ...", len(data), ", press \"Enter\" for continue.")
         input()
         continue
     while True:
-        print("How many",data[pos][0], "do you whant?")
-        q = int(input())
-        if q <= 0 or q > int(data[pos][2]):
+        q = int(input("How many " + data[pos][0] +" do you whant? "))
+        if q <= 0: 
+            break
+        if q > int(data[pos][2]):
             print("Wrong quantity.", "Is ready", data[pos][2], "now." )
         else:
-            print("cost", q * int(data[pos][1]))
-            input()
+            cost = q * int(data[pos][1])
+            order_line = f"{data[pos][0]:10} quantity:{q:3} cost:{cost:5}\n"
+            print("\n", order_line)
+            file.write(f"{data[pos][0]},{q},{cost}\n")
+            input("Press \"Enter\" for  continue")
             break
 
+printOrder()
     
--- homework/2024_10_29/Restaraunt/func.py
+++ homework/2024_10_29/Restaraunt/func.py
@@ -1,5 +1,5 @@
 def printMenu():
-    file = open('homework/2024_10_29/Restaraunt/data/food.csv', "r")
+    file = open('data/food.csv', "r")
     content = file.read()
 
     print("================== MENU =================")
@@ -10,6 +10,30 @@ def printMenu():
         pos += 1
         food_name, food_price, food_avail = line.split(",")
         data.append([food_name, food_price, food_avail])
-        print(f"{pos:2}) {food_name:>20} {food_price:10}")
+        print(f"{pos:2}) {food_name:<10} {food_price:10}")
 
     return data
+
+def printOrder():
+    file = open('data/order.csv', "r")
+    if file.__sizeof__ == 0:
+        return
+    content = file.read()
+
+    print("\n============== YOUR ORDER ============")
+    lines = content.split("\n")
+    
+    pos = 0
+    total = 0
+    for line in lines:
+        if len(line) == 0:
+            break
+        pos += 1
+        food_name, food_quantity, food_cost = line.split(",")
+        total += int(food_cost) 
+        order_line = f"{pos:2}) {food_name:10} quantity: {food_quantity:3} cost: {food_cost:5}"
+        print(order_line)
+
+    print("--------------------------------------")
+    print(f"                           Total:{total:5}\n")
+    return
